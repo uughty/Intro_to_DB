a264619b6c3a7d519a50c654b60eb803a09fe365
Malilo <sandramalilo21@gmail.com>
2025-06-24 22:07:41 +0300

    Fix foreign key syntax for Orders table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -31,8 +31,7 @@ CREATE TABLE Orders(
     order_id INT  PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
-
-    FOREIGN KEY (customer_id)  REFERENCES Customers (customer_id)
+    FOREIGN KEY (customer_id)  REFERENCES Customers(customer_id)
 
 
 );
